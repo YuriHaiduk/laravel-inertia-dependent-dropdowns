b50796acd69bdddd32822ffa0a34f070cc14d071
Yuri Haiduk <yurihaiduk@gmail.com>
2023-11-20 12:11:00 +0200

    hotfix

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,30 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\Airport;
 use App\Models\City;
 use App\Models\Country;
+use Illuminate\Http\JsonResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class FormController extends Controller
 {
 
-    public function index()
+    public function index(): Response
     {
-        $countries = Country::get(['id', 'name']);
+        $countries = Country::query()->get(['id', 'name']);
         return Inertia::render('Form/Index', [
             'countries' => $countries,
         ]);
 
     }
 
-    public function getCities($countryId)
+    public function getCities($countryId): JsonResponse
     {
-        $cities = City::where('country_id', $countryId)->get(['id', 'name']);
+        $cities = City::query()->where('country_id', $countryId)->get(['id', 'name']);
         return response()->json($cities);
 
     }
 
-    public function getAirports($cityId)
+    public function getAirports($cityId): JsonResponse
     {
-        $airports = Airport::where('city_id', $cityId)->get(['id', 'name']);
+        $airports = Airport::query()->where('city_id', $cityId)->get(['id', 'name']);
         return response()->json($airports);
 
     }
